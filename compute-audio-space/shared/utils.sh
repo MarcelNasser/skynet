e78be1af978efa94478832b996c143465aec6a1c
Marcel Ndeffo <marcel.nasser@live.fr>
2023-07-07 14:09:40 +0200

    refactor dependencies checkouts #3

--- compute-audio-space/shared/utils.sh
+++ compute-audio-space/shared/utils.sh
@@ -16,6 +16,7 @@ function ffmpeg-arch(){
 }
 
 function install-ffmpeg(){
+  info "  installing .. ffmpeg"
   OS=$(uname)
   case $OS in
     Linux) ffmpeg-arch "linux64" ".tar.xz" "tarfile" "sudo";;
@@ -28,17 +29,17 @@ function install-ffmpeg(){
 function install-package(){
   $PYTHON_BIN -m pip --version > /dev/null 2>/dev/null \
       ||  { info "pip not present. installing .."; curl https://bootstrap.pypa.io/get-pip.py  2>/dev/null | $PYTHON_BIN || exit 2; }
-  info "installing .. '$1'"
+  info "  installing .. '$1'"
   $PYTHON_BIN -m pip install scipy >/dev/null 2>/dev/null || { info "'$1' installation failed"; exit 2; }
 }
 
 function dependencies(){
   debug "=> dependencies: checking [ffmpeg/python]"
-  ffmpeg -version >/dev/null 2>/dev/null || { info "missing dependency ffmpeg. installing .."; install-ffmpeg; }
+  ffmpeg -version >/dev/null 2>/dev/null || { info "missing dependency ffmpeg."; install-ffmpeg; }
   $PYTHON_BIN --version | grep -E "Python 3.([1-9]|1[0-1])\..*" >/dev/null \
       ||  { info "Python version is not between 3.0 and 3.11 [detected: $($PYTHON_BIN --version)]"; exit 2; }
-  $PYTHON_BIN -c "import scipy" >/dev/null 2>/dev/null  || install-package scypi
-  $PYTHON_BIN -c "import matplotlib" >/dev/null 2>/dev/null || install-package matplotlib
-  $PYTHON_BIN -c "import numpy" >/dev/null 2>/dev/null || install-package numpy
+  $PYTHON_BIN -c "import scipy" >/dev/null 2>/dev/null  || { info "missing package 'scipy'."; install-package scypi; }
+  $PYTHON_BIN -c "import matplotlib" >/dev/null 2>/dev/null || { info "missing package 'matplotlib'."; install-package matplotlib; }
+  $PYTHON_BIN -c "import numpy" >/dev/null 2>/dev/null || { info "missing package 'numpy'."; install-package numpy; }
 }
 
