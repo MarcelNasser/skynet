6ca9992acbd5d40b7aff108e19046faa9da9bb8d
Marcel Ndeffo <marcel.nasser@live.fr>
2023-07-10 19:41:34 +0200

    fix: exit 2 scory

--- tests/chop-and-interpolate.sh
+++ tests/chop-and-interpolate.sh
@@ -12,7 +12,7 @@ bash "$ROOT_DIR"/transform-av/chop/run -s "$ROOT_DIR"/tests/data/audio/single-fi
 [ "$(find "$ROOT_DIR/tests/data/audio/single-file/.chopped" -maxdepth 1 -iname '*.y.wav'| wc -l)" -ne 2 ] && echo "number of chops <> 2" && error
 
 echo "#1 'interpolate' single audio"
-bash "$ROOT_DIR"/transform-av/interpolate/run -s "$ROOT_DIR"/tests/data/audio/single-file/.chopped -l 2  || error  && exit 2
+bash "$ROOT_DIR"/transform-av/interpolate/run -s "$ROOT_DIR"/tests/data/audio/single-file/.chopped -l 2  || error
 [ "$(find "$ROOT_DIR/tests/data/audio/single-file/.chopped/.interpolated" -maxdepth 1 -iname '*.x.wav'| wc -l)" -ne 2 ] && echo "number of interpolates <> 2" && error
 
 rm -r "$ROOT_DIR/tests/data/audio/single-file/.chopped/"
