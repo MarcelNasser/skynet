cb836feba847403780c995fefe21ce54994ca010
Marcel Ndeffo <marcel.nasser@live.fr>
2023-07-20 17:54:04 +0200

    fix exit code silencing

--- tests/scrap-yt.sh
+++ tests/scrap-yt.sh
@@ -5,8 +5,8 @@ ROOT_DIR=$(realpath "$(dirname "$0")"/..)
 source "$ROOT_DIR/transform-av/shared/utils.sh"
 
 
-
 # check 'docker' has at least 100 public repos on github
 echo "#0 'scrap-yt' with dummy file"
 bash "$ROOT_DIR"/download-yt/run -s "$ROOT_DIR"/tests/data/void/hello_world.txt -d tests/data/void >/dev/null || error
 echo "#alright!"
+
