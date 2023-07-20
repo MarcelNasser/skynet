bf1f35e43c0cba2c370a7f681c8c7802a11a4cfc
marcel ndeffo <47064690+MarcelNasser@users.noreply.github.com>
2023-07-20 19:00:38 +0200

    refactoring of tests (#12)

---
+++ download-yt/readme.md
@@ -0,0 +1,10 @@
+## Download youtube videos
+
+**run the script**
+```bash
+bash download-yt/run -u urls_list_txt -d target_directory
+
+Example:
+ 
+bash download-yt/run -u /c/Users/marce/Desktop/music.txt -d  /c/Users/marce/Desktop/
+```
---
+++ tests/scrap-yt.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# load utility functions
+ROOT_DIR=$(realpath "$(dirname "$0")"/..)
+
+source "$ROOT_DIR/transform-av/shared/utils.sh"
+
+
+# check 'docker' has at least 100 public repos on github
+echo "#0 'scrap-yt' with dummy file"
+bash "$ROOT_DIR"/download-yt/run -s "$ROOT_DIR"/tests/data/void/hello_world.txt -d "$ROOT_DIR"/tests/data/void >/dev/null || error
+echo "#alright!"
+
