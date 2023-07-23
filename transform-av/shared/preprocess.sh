91ac98eecd141dacf29c343a32021544bde344b8
marcel ndeffo <47064690+MarcelNasser@users.noreply.github.com>
2023-07-23 12:47:42 +0200

    fix-ffmpeg-alpine-pipeline (#16)

--- transform-av/shared/preprocess.sh
+++ transform-av/shared/preprocess.sh
@@ -71,10 +71,12 @@ function xx() {
 
 # check if all files in a directory are of a certain type
 function check_content(){
-  # shellcheck disable=SC2044
   LIST=$(find "$1" -maxdepth 1 -type f)
   [ -z "$LIST" ] && echo "error: empty folder '$1'" && error
   while read -r file; do
-    [ "$(file -b --mime-type "$file")" != "$2" ] && echo "error:$file is not of type $2" && error
+    if ! echo "${@:2}" | grep -q "$(file -b --mime-type "$file")"; then
+      echo "error:$file is not of type $2"
+      error
+    fi
   done <<< "$LIST"
 }
